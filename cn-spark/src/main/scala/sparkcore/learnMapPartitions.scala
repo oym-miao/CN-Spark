bd9e6c7bf556f90cb1733ac1ba27d322c9c14a20
oym <a952064610@qq.com>
2020-01-25 22:10:12 +0800

    测试mapPartition 并且贴上打印的结果

--- cn-spark/src/main/scala/sparkcore/learnMapPartitions.scala
+++ cn-spark/src/main/scala/sparkcore/learnMapPartitions.scala
@@ -6,26 +6,40 @@ object learnMapPartitions {
   def main(args: Array[String]): Unit = {
     val conf=new SparkConf()
     conf.setAppName("learnMapPartitions")
-    conf.setMaster("local[4]")
+    conf.setMaster("local[*]")
 
     val sc =new SparkContext(conf)
     val textFileRDD=sc.textFile("in/README.md")
 
     //mapPartitions可以对一个RDD中所有的分区进行遍历
     //map每一个分区，然后再map分区中的每一个元素,这个partition是一个集合
-    //优点
     val mapPartitionRDD=textFileRDD.mapPartitions(partition => {
       partition.map(line =>line.toUpperCase)
     })
 
     //foreach是一个没有返回值的action
-//    mapPartitionRDD.foreach(line =>println(line))
+    mapPartitionRDD.foreach(line =>println(line))
+
+    /**
+      * 输出结果， 单词全部都转成大写了， 但是顺序有的是不对的，因为上面设置的是local[*]，多个线程并发读取
+      *
+      * # APACHE SPARK
+      * TO RUN ONE OF THEM, USE `./BIN/RUN-EXAMPLE <CLASS> [PARAMS]`. FOR EXAMPLE:
+      *
+      *
+      * SPARK IS A FAST AND GENERAL CLUSTER COMPUTING SYSTEM FOR BIG DATA. IT PROVIDES
+      * ./BIN/RUN-EXAMPLE SPARKPI
+      * HIGH-LEVEL APIS IN SCALA, JAVA, PYTHON, AND R, AND AN OPTIMIZED ENGINE THAT
+      */
+
+
+
 
     val mapPartitionsWithIndexRDD=textFileRDD.mapPartitionsWithIndex((index,partition) => {
       partition.map(line =>index +" : "+line.toUpperCase)
     })
 
-//    mapPartitionsWithIndexRDD.foreach(line =>println(line))
+    //mapPartitionsWithIndexRDD.foreach(line =>println(line))
 
     val data = sc.parallelize(Array(('A',1),('b',2)))
     val data2 =sc.parallelize(Array(('A',4),('A',6),('b',7),('c',3),('c',8)))
