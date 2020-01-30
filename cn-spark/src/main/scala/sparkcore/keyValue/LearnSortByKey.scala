a512c6d859f9a0b845e2d601fb4f355c8a112368
oym <a952064610@qq.com>
2020-01-30 21:25:10 +0800

    测试SortByKey

---
+++ cn-spark/src/main/scala/sparkcore/keyValue/LearnSortByKey.scala
@@ -0,0 +1,28 @@
+package sparkcore.keyValue
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object LearnSortByKey {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf: SparkConf = new SparkConf().setAppName("LearnSortByKey").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val dataRdd: RDD[(Int, String)] = sc.makeRDD(List((1,"张三"),(2,"李四"),(4,"赵钱"),(3,"王五")))
+
+    val sortByKeyRDD: RDD[(Int, String)] = dataRdd.sortByKey(true)
+
+    sortByKeyRDD.collect().foreach(print)
+
+    /**
+      * 输出结果
+      * (1,张三)(2,李四)(3,王五)(4,赵钱)
+      */
+
+
+  }
+
+}
